621efaa9417b7c02b6e1531b07a8a7e8df8b1855
Gurmehar Singh <gurmehar@gmail.com>
2021-02-17 09:21:58 -0800

    security update.

--- main.js
+++ main.js
@@ -12,29 +12,74 @@ const {URLSearchParams} = require('url')
 
 const app = express()
 const port = 80
-const host = 'localhost'
+const host = '192.168.86.35'
+
+const accesscodes = ['!mgvs0_55lk', "!hjys_6kj3l:"]
+
+function dealWithMalformed(res){
+	//rickroll those hackers
+	res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+}
+
+function checkFileExists(dir, file){
+	var files = fs.readdirSync(dir);
+	if (files.includes(file)){
+		return true
+	}
+	return false
+}
 
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-	res.sendFile("public/templates/main.html", {root: __dirname})
+	res.sendFile("public/templates/landing.html", {root: __dirname})
 })
 
-app.get("/load", (req, res) => {
+app.get("/access", (req, res) => {
+	res.sendFile("public/templates/access.html", {root: __dirname})
+})
+
+app.get("/*", (req, res, next) => {
 	var url = req.url.split("?")[1];
 	var urlParams = new URLSearchParams(url);
-	var x = urlParams.get("x")
-	var y = urlParams.get("y")
-	var file = urlParams.get("file")
-	res.sendFile("saves/" + file + "/chunk_" + x + "_" + y +".txt", {root: __dirname})
+	var codeparam = urlParams.get("accesscode")
+	if (accesscodes.includes(codeparam)){
+		return next();
+	}
+	else{
+		dealWithMalformed(res)
+	}
+})
+
+app.get("/game", (req, res) => {
+	var ip = req.connection.remoteAddress;
+	console.log("Connection to '/game' at ", new Date(new Date().toUTCString()), " from ", ip)
+	res.sendFile("public/templates/main.html", {root: __dirname})
 })
 
 app.get("/gettile", (req, res) => {
 	var url = req.url.split("?")[1];
 	var urlParams = new URLSearchParams(url);
 	var x = urlParams.get("x")
-	var y = urlParams.get("y")	
+	var y = urlParams.get("y")
+
+	var malformed = false
+
+	if (isNaN(x)){
+		malformed = true
+	}
+
 	var file = urlParams.get("file")
+
+	if (!checkFileExists('saves/', file)){
+		malformed = true
+	}
+
+	//rickroll the hackers
+	if (malformed){
+		dealWithMalformed(res)
+		return
+	}
 
 	var db = new sqlite3.Database("saves/" + file + "/world.db")
 	var command = "SELECT * FROM world WHERE tilename='" + x + "_" + y + "'"
@@ -51,6 +96,10 @@ app.get("/seed", (req, res) => {
 	var url = req.url.split("?")[1];
 	var urlParams = new URLSearchParams(url);
 	var file = urlParams.get("file")
+	if (!checkFileExists('saves/', file)){
+		dealWithMalformed(res)
+		return
+	}
 	res.sendFile("saves/" + file + "/seed.txt", {root: __dirname})	
 })
 
@@ -58,6 +107,10 @@ app.get("/explored", (req, res) => {
 	var url = req.url.split("?")[1];
 	var urlParams = new URLSearchParams(url);
 	var file = urlParams.get("file")
+	if (!checkFileExists('saves/', file)){
+		dealWithMalformed(res)
+		return
+	}
 	res.sendFile("saves/" + file + "/explored.txt", {root: __dirname})	
 })
 
@@ -65,6 +118,10 @@ app.get("/units", (req, res) => {
 	var url = req.url.split("?")[1];
 	var urlParams = new URLSearchParams(url);
 	var file = urlParams.get("file")
+	if (!checkFileExists('saves/', file)){
+		dealWithMalformed(res)
+		return
+	}
 	res.sendFile("saves/" + file + "/units.txt", {root: __dirname})	
 })
 
