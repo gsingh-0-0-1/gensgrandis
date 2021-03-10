12af4af07b506b0825c859bbde208ad18da26c67
Gurmehar Singh <gurmehar@gmail.com>
2021-03-09 23:29:49 -0800

    new song, updated audio interface

--- public/templates/audioutils.js
+++ public/templates/audioutils.js
@@ -9,3 +9,26 @@ function loopAudio(obj){
 		obj.style.backgroundColor = "#bbb"
 	}
 }
+
+function addSong(id, name){
+	var cont = document.getElementById("music_holder")
+	var temp = document.getElementById("audio_template")
+
+	var n = temp.cloneNode(true)
+	n.id = id
+
+
+	n.children[0].id = id + "_player"
+	n.children[0].children[0].src = "/sounds/" + id + ".mp3"
+
+	n.children[1].children[0].id = id + "_songname"
+
+	n.children[1].children[0].textContent = '"' + name + '"'
+
+	n.style.display = "initial"
+
+	cont.appendChild(n)
+}
+
+addSong("gg_1", "Beginnings")
+addSong("gg_2", "Sailing")
