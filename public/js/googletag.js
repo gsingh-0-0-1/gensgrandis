b76fc40d0c25008981663e57eecbb7855ef37f13
Gurmehar Singh <gurmehar@gmail.com>
2021-04-24 00:40:48 -0700

    changed google analytics tag adding, it wont track hits to my development server now

---
+++ public/js/googletag.js
@@ -0,0 +1,9 @@
+
+if (window.location.hostname.includes("gensgrandis")){
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-131520225-3');
+
+}
