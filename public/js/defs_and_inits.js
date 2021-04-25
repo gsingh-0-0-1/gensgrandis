fba639d345cc3e5582f890c86ad54dad45540c63
Gurmehar Singh <gurmehar@gmail.com>
2021-04-25 10:08:17 -0700

    changed game soundtrack order / song picks

--- public/js/defs_and_inits.js
+++ public/js/defs_and_inits.js
@@ -31,7 +31,7 @@ addSong("gg_5", "Gens Grandis", cont)
 addSong("gg_6", "Sly", cont)
 addSong("gg_8", "Astra", cont)
 
-var song_ids_to_play = ["gg_1", "gg_2", "gg_8", "gg_4", "gg_5"]
+var song_ids_to_play = ["gg_5", "gg_4", "gg_8", "gg_2"]
 
 var multi = false
 
